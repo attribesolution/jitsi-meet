35b59895222ef253ee274d0d7d029652bda1691b
Syed M. Taha <s.m.taha10@gmail.com>
2018-10-26 04:38:09 +0500

    Change http to https

--- react/features/welcome/components/WelcomePage.web.js
+++ react/features/welcome/components/WelcomePage.web.js
@@ -86,7 +86,7 @@ class WelcomePage extends AbstractWelcomePage {
         if (!localStorage.getItem('token')) {
             console.log('Hello world');
 
-            window.location.href = 'https://meetauth.attribes.com';
+            window.location.href = 'http://meetauth.attribes.com';
 
         }
     }
